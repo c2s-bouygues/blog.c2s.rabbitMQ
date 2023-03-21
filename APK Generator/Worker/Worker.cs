91fb37ad86d0134bcf9dc178a04b9258d8d54062
MathMirage <127861164+MathMirage@users.noreply.github.com>
2023-03-21 10:44:08 +0100

    ISO Articles

--- APK Generator/Worker/Worker.cs
+++ APK Generator/Worker/Worker.cs
@@ -1,65 +1,88 @@
-﻿using System.Diagnostics;
+﻿using System.IO.Compression;
+using System.Diagnostics;
 using System.Text;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
 var factory = new ConnectionFactory() { HostName = "localhost" };
-using (var connection = factory.CreateConnection())
-using (var channel = connection.CreateModel())
-{
-    channel.QueueDeclare(queue: "apk-generate",
-                         durable: true,
-                         exclusive: false,
-                         autoDelete: false,
-                         arguments: null);
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
 
-    var consumer = new EventingBasicConsumer(channel);
-    consumer.Received += (model, ea) =>
-    {
-        var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+channel.QueueDeclare(queue: "apk-to-generate", durable: true, exclusive: false, autoDelete: false, arguments: null);
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(queue: "apk-to-generate",
+                    autoAck: false,
+                    consumer: consumer);
+Console.WriteLine(" [x] Awaiting RPC requests");
+    
+consumer.Received += (model, ea) =>
+{
+    // Propriétés du message
+    string response = string.Empty;
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);    
+    
+    var props = ea.BasicProperties;
+    var replyProps = channel.CreateBasicProperties();
+    replyProps.CorrelationId = props.CorrelationId;
+
+    // Sépare le message en arguments
+    var args = message.Split(' ');
+    var path = args[0];
+    var script = args[1];
+    var extractPath = @$"/C/{path}";
+
+    // Décompression du fichier zip
+    byte[] fileBytes = ea.Body.ToArray();
+    File.WriteAllBytes(path, fileBytes);
+    ZipFile.ExtractToDirectory(path, extractPath);
+
+    try{
         Console.WriteLine(" [x] Received {0}", message);
-        
-        var args = message.Split(' '); // sépare le message en arguments
-        var path = args[0];
-        var script = args[1];
-        
-        // Exécute la commande "yarn run" pour générer l'APK avec le chemin et le script spécifiés en paramètres
-        var process = new Process
+        // Exécute la commande yarn install pour installer les dépendances puis "yarn run" pour générer l'APK avec le chemin et le script spécifiés en paramètres
+        var apkprocess = new Process
         {
             StartInfo = new ProcessStartInfo
             {
                 FileName = "cmd.exe",
-                Arguments = $"/C cd {path} && yarn run {script}",
+                Arguments = $"/C cd {extractPath} && yarn install && yarn run {script}",
                 UseShellExecute = false,
                 RedirectStandardOutput = true,
                 CreateNoWindow = true
             }
         };
-        process.Start();
-        string output = process.StandardOutput.ReadToEnd();
-        process.WaitForExit();
+        apkprocess.Start();
+        string output = apkprocess.StandardOutput.ReadToEnd();
+        apkprocess.WaitForExit();
         Console.WriteLine(output);
-        
-        // Renvoie le fichier APK généré en réponse
+    }
+
+    // Gestion d'erreur
+    catch (Exception e)
+    {
+        Console.WriteLine($" [.] {e.Message}");
+        response = string.Empty;
+    }
+
+    finally
+    {
+        // Renvoie du fichier APK généré en réponse
         var apkFile = $"{path}\\app-release.apk";
+
         if (File.Exists(apkFile))
         {
-            var fileBytes = File.ReadAllBytes(apkFile);
-            channel.BasicPublish(exchange: "",
-                                 routingKey: ea.BasicProperties.ReplyTo,
-                                 basicProperties: new BasicProperties
-                                 {
-                                     CorrelationId = ea.BasicProperties.CorrelationId
-                                 },
-                                 body: fileBytes);
+            var fileB = File.ReadAllBytes(apkFile);
+            channel.BasicPublish(exchange: "", routingKey: props.ReplyTo, basicProperties: replyProps, body: fileB);
+            channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
             Console.WriteLine(" [x] Sent apk file");
         }
+
         else
         {
             Console.WriteLine(" [x] Failed to generate apk file");
         }
-    };
-    channel.BasicConsume(queue: "apk-generate",
-                         autoAck: true,
-                         consumer: consumer);
-}
+    }    
+};
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
